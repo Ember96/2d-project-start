435fb7486567d0dafb020b80cc8921c8a14c0019
Ember96 <gnomeaddict96@gmail.com>
2024-09-15 23:21:04 -0700

    broken hook attempt

--- gun.gd
+++ gun.gd
@@ -4,4 +4,6 @@ func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
-		look_at(target_enemy.global_position)
+		#look_at(target_enemy.global_position)
+		var cursor_position = get_global_mouse_position()
+		look_at(cursor_position)
--- player.gd
+++ player.gd
@@ -2,9 +2,19 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var mouse_position = get_global_mouse_position()
+	var pressed = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
 	velocity = direction * 500
 	move_and_slide()
 	if velocity.length() > 0:
 		%HappyBoo.play_walk_animation()
+	# Get the global mouse position
+	if pressed:
+		direction = mouse_position
+		velocity = direction * 2
+		move_and_slide()
 	else:
 		%HappyBoo.play_idle_animation()
+
+		
+
